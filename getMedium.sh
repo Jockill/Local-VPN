b5ccafb72b6e71a4853d05580eef1cbe34d00895
JOCHYMSKI Hugo <hugo.jochymski@etu.unistra.fr>
2021-11-04 14:36:52 +0100

    Ajout de getMedium.sh
    Permet de récupérer la dernière version du medium depuis le depot distant

---
+++ getMedium.sh
@@ -0,0 +1,4 @@
+wget https://git.unistra.fr/alfroy/projet_algo_reseau2021/-/archive/master/projet_algo_reseau2021-master.tar.gz
+tar -xf projet_algo_reseau2021-master.tar.gz
+mv projet_algo_reseau2021-master/medium.py .
+rm -rf projet_algo_reseau2021-master projet_algo_reseau2021-master.tar.gz
