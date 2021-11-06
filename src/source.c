9c183f47cad3e1842f5d1be73802f5900e207bc5
Samuel LEBOT <samuel.lebot@etu.unistra.fr>
2021-11-06 19:36:11 +0100

    ajout du three way hand shake dans source.c

--- src/source.c
+++ src/source.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "utils.c"
 
 void check_args_src(int argc, char** argv){
@@ -18,7 +19,7 @@ void check_args_src(int argc, char** argv){
         exit(1);
     }
     int mode = strtol(argv[1],NULL,0);
-    if(mode != 1 && mode != 2){
+    if(mode != STOP_N_WAIT && mode != GO_BACK_N){
         fprintf(stderr,"Erreur : mode non valide.\n");
         fprintf(stderr,"Rappel mode : 1=\'stop and wait\', 2=\'go back n\'.\n");
         exit(1);
@@ -34,3 +35,92 @@ void check_args_src(int argc, char** argv){
     return;
 }
 
+int negociation_src(int sockServeur, int sockClient,struct sockaddr_in* serveur,struct sockaddr_in* client, int mode, fenetre* fen){
+    short numA =(short) rand();
+    short numB;
+    ssize_t tmp;
+
+    if(mode != GO_BACK_N || mode != STOP_N_WAIT){
+        fprintf(stderr,"mode incorrecte\n");
+        close(sockClient);
+        close(sockServeur);
+        exit(1);
+    }
+
+    paquet premierHandShake = cree_paquet(0,SYN,numA,0,0,mode-1,NULL);
+    //On donne le mode de la communication en utilisisant habilement le champ taille fenetre
+    //Si la taille de la fenetre est a 0 quand le client ouvre la connection alors
+    //la communication sera en stop and wait et si elle est a 1 alors go back n
+    paquet ack;
+    fd_set acquittement;
+    int ack1Recu = 0;
+    while(!ack1Recu){
+
+        if((tmp = sendto(sockServeur,(void*)&premierHandShake,
+            52,0,(struct sockaddr*)serveur,sizeof(*serveur)) == -1)){
+            tue_moi("sendto",2,sockServeur,sockClient);
+        }
+        if(tmp !=52) continue; //si on a pas reussi a toute envoyer on renvoie
+
+        FD_ZERO(&acquittement);
+        FD_SET(sockClient,&acquittement);
+        struct timeval timer = {1,0};
+
+        if(select(FD_SETSIZE,&acquittement,NULL,NULL,&timer) ==-1){
+            tue_moi("sendto",2,sockServeur,sockClient);
+        }
+
+        if(FD_ISSET(sockClient,&acquittement)){
+            if((tmp = recv(sockClient,(void *)&ack,52,0))==-1){
+                tue_moi("sendto",2,sockServeur,sockClient);
+            }
+            if(tmp != 52) continue; //si on a pas reussi a tout recevoir
+            if((ack.type & (SYN|ACK))==0) continue;
+            if(ack.numAck != numA+1) continue;
+            numB = ack.numSeq;
+            fen->tailleEnvoi= ack.tailleFenetre;
+            ack1Recu=1;
+        }
+    }
+    paquet dernierHandShake = cree_paquet(0,ACK,numA+1,numB+1,0,0,NULL);
+    if((tmp = sendto(sockServeur,(void*)&dernierHandShake,
+            52,0,(struct sockaddr*)serveur,sizeof(*serveur)) == -1)){
+            tue_moi("sendto",2,sockServeur,sockClient);
+    }
+    if(tmp !=52){
+        close(sockClient);
+        close(sockServeur);
+        fprintf(stderr,"plantage handsake\n");
+        exit(1);
+    }
+    
+
+    return 0;
+    
+}
+
+int main(int argc, char** argv){
+
+    int mode;
+    fenetre fen;
+    fen.debut = fen.fin = 0;
+
+
+    struct sockaddr_in serveur, client;
+    
+    srand(time(NULL));
+    check_args_src(argc,argv);
+    init_addr(&serveur,argv[2],argv[4]);
+    init_addr(&client,NULL,argv[3]);
+
+    int sockServeur = socket(PF_INET,SOCK_DGRAM,0); // socket avec laquelle j'envoie
+    if(sockServeur == -1){
+
+    }
+    int sockClient = socket(PF_INET,SOCK_DGRAM,0); // socket avec laquelle je reçoit
+
+    if(bind(sockClient,(struct sockaddr*)&client,sizeof(client))==-1){
+        tue_moi("bind",0);
+    }
+
+}
