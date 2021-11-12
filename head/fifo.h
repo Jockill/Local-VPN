44fe27118ba62b11cb1adf399dac9ad0bec25b73
JOCHYMSKI Hugo <jochymski@etu.unistra.fr>
2021-11-12 17:39:09 +0100

    Ajout des fonctionalités de FIFO
    + fifo.c
    + fifo.h

---
+++ head/fifo.h
@@ -0,0 +1,37 @@
+#ifndef __FIFO_H__
+#define __FIFO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct maillon
+{
+        paquet* element;
+        maillon* precedent;
+        maillon* suivant;
+} maillon;
+
+
+typedef struct fifo
+{
+        maillon* debut;
+        maillon* fin;
+        ssize_t taille = 0;
+} fifo;
+
+
+fifo* cree_fifo();
+
+int push_fifo(fifo* f, paquet* p);
+
+maillon* premier_fifo(fifo* f);
+
+maillon* pop_fifo(fifo* f);
+
+int est_vide_fifo(fifo* f);
+
+int envoi_fifo(fifo* f, int socket, struct sockaddr_in* dest);
+
+void affiche_fifo(fifo* f);
+
+#endif
---
+++ src/fifo.c
@@ -0,0 +1,77 @@
+#include "../head/fifo.h"
+
+fifo* cree_fifo()
+{
+        fifo* file = (fifo*)malloc(sizeof(fifo));
+        if (file == NULL)
+                return NULL;
+        file->debut = NULL;
+        file->fin = NULL;
+        file->taille = 0;
+        return file;
+}
+
+int push_fifo(fifo* f, paquet* p)
+{
+        maillon* fin = f->fin;
+
+        maillon* new = (maillon*)malloc(sizeof(maillon));
+        if (new == NULL)
+                return -1;
+        new->element = p;
+        new->precedent = fin;
+        new->suivant = NULL;
+
+        if (fin != NULL)
+                fin->suivant = new;
+
+        if (f->taille == 0)
+                f->debut = new;
+        f->fin = new;
+        (f->taille)++;
+        return 0;
+}
+
+maillon* premier_fifo(fifo* f) {return f->debut;}
+
+maillon* pop_fifo(fifo* f)
+{
+        maillon* tete = f->debut;
+        maillon* nouveauDebut = f->debut->suivant;
+
+        printf("nouvD = %p\n", nouveauDebut);
+
+        f->debut = nouveauDebut;
+        (f->taille)--;
+        if (f->taille == 0)
+                f->fin = NULL;
+
+        return tete;
+}
+
+int est_vide_fifo(fifo* f) {return f->taille>0 ? 1:0;}
+
+int envoi_fifo(fifo* f, int socket, struct sockaddr_in* dest)
+{
+        maillon* courant = f->debut;
+        for (ssize_t i=0; i<f->taille; i++)
+        {
+                if (courant == NULL)
+                {
+                        fprintf(stderr, "envoi_fifo: La file ne semble pas correctement chainée.\n");
+                        return -1;
+                }
+                envoi_paquet(socket, dest, courant);
+                courant = courant->suivant;
+        }
+        return 0;
+}
+
+void affiche_fifo(fifo* f)
+{
+        printf("============= FILE ============\n");
+        printf("Debut : %p\n", f->debut);
+        printf("Fin:    %p\n", f->fin);
+        printf("Taille: %ld\n", f->taille);
+        printf("===============================\n");
+}
