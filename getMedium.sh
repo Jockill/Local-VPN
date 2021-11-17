3a6f9da99251f100cf772686e9ef7c480282b97e
JOCHYMSKI Hugo <hugo.jochymski@etu.unistra.fr>
2021-11-17 15:25:56 +0100

    Patch getMedium

--- getMedium.sh
+++ getMedium.sh
@@ -1,11 +1,6 @@
 rm -f *.py;
 
-if [ "$1" = "PROF" ]
-then
-        wget -nv https://git.unistra.fr/alfroy/projet_algo_reseau2021/-/archive/master/projet_algo_reseau2021-master.tar.gz
-        tar -xf projet_algo_reseau2021-master.tar.gz;
-        mv projet_algo_reseau2021-master/medium.py .;
-        rm -rf projet_algo_reseau2021-master projet_algo_reseau2021-master.tar.gz;
-else
-        wget -nv https://git.unistra.fr/pvcmeyer/projet_algo_reseau2021/-/raw/master/medium.py
-fi
+wget -nv https://git.unistra.fr/alfroy/projet_algo_reseau2021/-/archive/master/projet_algo_reseau2021-master.tar.gz
+tar -xf projet_algo_reseau2021-master.tar.gz;
+mv projet_algo_reseau2021-master/medium.py .;
+rm -rf projet_algo_reseau2021-master projet_algo_reseau2021-master.tar.gz;
